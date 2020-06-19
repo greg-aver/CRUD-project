49f1000be0a2406a4088bac5f3442919488d4db6
Gregory Averianov <gregory.averianov@gmail.com>
2020-06-19 15:51:41 +0300

    add createDAO()

--- src/main/java/factory/UserDAOFactory.java
+++ src/main/java/factory/UserDAOFactory.java
@@ -3,9 +3,35 @@ package factory;
 import DAO.UserDAO;
 import model.User;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public abstract class UserDAOFactory {
 
+   enum DAO {
+       JDBC,
+       Hibernate
+   }
 
+    public void createDAO() {
+        Properties properties = new Properties();
+        try (FileInputStream inputStream = new FileInputStream("db.properties")) {
+            properties.load(inputStream);
+            DAO daotype = DAO.valueOf(properties.getProperty("daotype"));
+            switch (daotype) {
+                case JDBC:
+                    break;
+                case Hibernate:
+                    break;
+            }
+
+            String qe = String.valueOf(DAO.Hibernate);
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+
+    }
 
     abstract public UserDAO<User> realizationDAO();
 }
